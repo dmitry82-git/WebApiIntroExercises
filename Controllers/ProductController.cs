b71ccdc1197516bd1461909b13825f3bd48697c9
Dimitri Trotsenko <53246496+dmitry82-git@users.noreply.github.com>
2026-09-16 10:25:00 +1000

    Complete Task 03 redirect endpoints

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -12,4 +12,18 @@ public class ProductController : ControllerBase
         return $"Product: {name}, Quantity: {quantity}";
     }
 
+    [HttpGet("description/new")]
+    public string GetNewDescription()
+    {
+        return "You have been redirected to the new description page for a product!";
+    }
+
+
+    [HttpGet("description/old")]
+    public IActionResult GetOldDescription()
+    {
+        return Redirect("/api/product/description/new");
+    }
+
+
 }
